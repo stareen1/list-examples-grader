b3b99870800b923028d8ca30da296c15eced2516
stareen1 <stareen@ucsd.edu>
2022-11-09 18:13:28 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,3 +5,12 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+
+if [[ ! -f ListExamples.java ]]
+then 
+    echo "ListExamples.java does not exist"
+    exit 1
+fi
+
+cp TestListExamples.java
